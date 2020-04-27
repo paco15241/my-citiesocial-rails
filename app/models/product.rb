078a8dcad442bdd269cb1c13a989aab9f48c8cdb
Paco <paco15241@gmail.com>
2020-04-27 20:39:46 +0800

    22 - add Action Text - 2.22

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   acts_as_paranoid
   include CodeGenerator
 
+  has_rich_text :description
+  
   validates :name, presence: true
   validates :list_price, :sell_price, numericality: { greater_then: 0, allow_nil: true }
   validates :code, uniqueness: true
